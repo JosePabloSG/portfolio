3bc5dc5d57d61b6b937100b8777a3fb043059641
José Pablo <suarezgomezjosepablo03@gmail.com>
2024-11-14 17:34:49 -0600

    refactor: reorder export statements in grid index file

--- components/grid/index.ts
+++ components/grid/index.ts
@@ -1,7 +1,7 @@
-export { default as Theme } from './theme';
-export { default as Spotify } from './spotify';
-export { default as Location } from './location';
-export { default as Github } from './github';
-export { default as Description } from './description';
-export { default as Contact } from './contact';
 export { default as Article } from './article';
+export { default as Contact } from './contact';
+export { default as Description } from './description';
+export { default as Location } from './location';
+export { default as Spotify } from './spotify';
+export { default as Theme } from './theme';
+export { default as Github } from './Github';
